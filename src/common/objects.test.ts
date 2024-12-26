4bc6d17f0980bb76240325a3d161c2a64cfa95f6
vagrosu <vagrosu@yahoo.com>
2024-12-26 02:25:29 +0200

    Add omitKeys function

--- src/common/objects.test.ts
+++ src/common/objects.test.ts
@@ -1,4 +1,4 @@
-import { isObject, isObjectEmpty } from "./objects";
+import { isObject, isObjectEmpty, omitKeys } from "./objects";
 
 describe("isObject", () => {
   it("should return true for objects", () => {
@@ -32,5 +32,28 @@ describe("isObjectEmpty", () => {
   it("should return false for non-empty objects", () => {
     expect(isObjectEmpty({ key: "value" })).toBe(false);
     expect(isObjectEmpty({ key: "value", key2: [1, "2", {}] })).toBe(false);
+  });
+});
+
+describe("omitKeys", () => {
+  it("should return a new object with the specified keys omitted", () => {
+    const obj = { key1: "value1", key2: "value2", key3: "value3" };
+
+    expect(omitKeys(obj, [])).toEqual(obj);
+    expect(omitKeys(obj, ["key1"])).toEqual({ key2: "value2", key3: "value3" });
+    expect(omitKeys(obj, ["key2", "key3"])).toEqual({ key1: "value1" });
+    expect(omitKeys(obj, ["key1", "key3"])).toEqual({ key2: "value2" });
+    expect(omitKeys(obj, ["key1", "key2", "key3"])).toEqual({});
+    expect(omitKeys(obj, ["key1", "key2", "key3", "key4"] as any)).toEqual({});
+    expect(omitKeys(obj, ["key4", "key5", "key6"] as any)).toEqual(obj);
+  });
+
+  it("should not modify the original object", () => {
+    const obj = { key1: "value1", key2: "value2", key3: "value3" };
+    const objCopy = { ...obj };
+
+    omitKeys(obj, ["key1"]);
+
+    expect(obj).toEqual(objCopy);
   });
 });
--- src/common/objects.ts
+++ src/common/objects.ts
@@ -5,3 +5,10 @@ export const isObject = (obj: unknown): obj is {} => {
 export const isObjectEmpty = (obj: {}) => {
   return Object.keys(obj).length === 0;
 };
+
+export const omitKeys = <T extends {}>(obj: T, keys: (keyof T)[]): Partial<T> => {
+  const newObj = { ...obj };
+  keys.forEach((key) => delete newObj[key]);
+
+  return newObj;
+};
