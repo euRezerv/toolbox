404b40bcea3ef20e11486f691d1db01bc7124336
vagrosu <vagrosu@yahoo.com>
2024-12-26 20:35:46 +0200

    Improve omitKeys function documentation

--- src/common/objects.ts
+++ src/common/objects.ts
@@ -6,9 +6,14 @@ export const isObjectEmpty = (obj: {}) => {
   return Object.keys(obj).length === 0;
 };
 
-export const omitKeys = <T extends {}>(obj: T, keys: (keyof T)[]): Partial<T> => {
+/**
+ * @param obj Object to omit keys from
+ * @param keysToOmit Array of keys to omit from the object
+ * @returns Shallow copy of the object with the specified keys omitted
+ */
+export const omitKeys = <T extends {}>(obj: T, keysToOmit: (keyof T)[]): Partial<T> => {
   const newObj = { ...obj };
-  keys.forEach((key) => delete newObj[key]);
+  keysToOmit.forEach((key) => delete newObj[key]);
 
   return newObj;
 };
