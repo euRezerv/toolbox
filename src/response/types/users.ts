930420e26fecb7a145c410e6602c789a9d1f71da
vagrosu <vagrosu@yahoo.com>
2025-01-03 00:17:31 +0200

    Add GetAuthUserResponseType to include user authentication details

--- src/response/types/users.ts
+++ src/response/types/users.ts
@@ -1,6 +1,23 @@
 import { AllOrNothing } from "src/common/typescript";
 import { StandardResponseType } from "./types";
 
+export type GetAuthUserResponseType = StandardResponseType<{
+  user: {
+    id: string;
+    isProfileComplete: boolean;
+    updatedAt: string;
+    createdAt: string;
+  } & AllOrNothing<{
+    givenName: string;
+    familyName: string;
+    email: string;
+    isEmailVerified: boolean;
+    phoneNumber: string;
+    isPhoneVerified: boolean;
+    isSystemAdmin: boolean;
+  }>;
+}>;
+
 export type GetUserByIdResponseType = StandardResponseType<{
   user: {
     id: string;
