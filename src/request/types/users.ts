090e8f08aaa8f583659434af76bfae4e2438fc80
vagrosu <vagrosu@yahoo.com>
2025-01-04 18:44:25 +0200

    Add CreateOrUpdateUserLocalProfileRequestType for user profile management

--- src/request/types/users.ts
+++ src/request/types/users.ts
@@ -1,3 +1,12 @@
 export type GetUserByIdRequestType = {
   id: string;
 };
+
+export type CreateOrUpdateUserLocalProfileRequestType = {
+  userId?: string;
+  givenName: string;
+  familyName: string;
+  email: string;
+  phoneNumberCountryISO: string;
+  phoneNumber: string;
+};
