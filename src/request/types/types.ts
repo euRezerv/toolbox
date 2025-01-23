bac248a70d81323d04c7648d279d8b8ed41c40c7
vagrosu <vagrosu@yahoo.com>
2025-01-23 11:12:21 +0200

    Add request types for handling path, body, and query parameters

--- src/request/types/types.ts
+++ src/request/types/types.ts
@@ -3,6 +3,10 @@ import { Request } from "express";
 export type RequestWithPath<P> = Request<P, never, never>;
 export type RequestWithBody<B> = Request<never, never, B>;
 export type RequestWithQuery<Q> = Request<never, never, never, Q>;
+export type RequestWithPathAndBody<P, B> = Request<P, never, B>;
+export type RequestWithPathAndQuery<P, Q> = Request<P, never, never, Q>;
+export type RequestWithBodyAndQuery<B, Q> = Request<never, never, B, Q>;
+export type RequestWithPathAndBodyAndQuery<P, B, Q> = Request<P, never, B, Q>;
 
 export type PaginationRequestType = {
   query: {
